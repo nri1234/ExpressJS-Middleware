02872c54976dc12354195c8b7d0e417bf3210f85
Norbert Mielczarek <nri1234.nm@gmail.com>
2019-08-22 06:15:47 +0200

    Made changes

--- server.js
+++ server.js
@@ -1,31 +1,33 @@
-var express = require('express');
-bodyParser = require('body-parser');
+var express = require("express");
+bodyParser = require("body-parser");
 var app = express();
 app.use(bodyParser.json());
 
-app.use('/',function(req, res, next){
-    console.log('Authentication of user');
+app.use("/", function(req, res, next) {
+    console.log("Authentication of user");
     next();
 });
-app.use('/',function(req, res, next){
-    console.log('Checking your details...');
+app.use("/", function(req, res, next) {
+    console.log("Checking your details...");
     next();
 });
-app.use('/store',function(req, res, next){
-    console.log('I am an intermediary when requesting /store.');
+app.use("/store", function(req, res, next) {
+    console.log("I am an intermediary when requesting /store.");
     next();
 });
 
-app.get('/', function (req, res) {
-    res.send('Hello world');
+app.get("/", function(req, res) {
+    res.send("Hello world");
 });
 
-app.get('/store', function (req, res) {
-    res.send('This is shop');
+app.get("/store", function(req, res) {
+    res.send("This is shop");
 });
 
-app.listen(3000);
+app.listen(3000, "127.0.0.1", function(err) {
+    console.log("Opened server on Store");
+});
 
-app.use(function (req, res, next) {
-    res.status(404).send('Sorry, we could not find what you want!')
+app.use(function(req, res, next) {
+    res.status(404).send("Sorry, we could not find what you want!");
 });
